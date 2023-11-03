c7c0f9f8fa78eceba0accbe21d9261bce2f19129
Your Name <you@example.com>
2023-11-04 00:46:01 +0530

    Add untracked files to the repository

--- main/views.py
+++ main/views.py
@@ -9,23 +9,15 @@ def dashboard_view(request):
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import ProductMst
 
-# Existing views for add_product and admin_dashboard ...
 
 def edit_product(request, product_id):
     product = ProductMst.objects.get(pk=product_id)
-
     if request.method == 'POST':
-        # Update the product with the submitted data
-       
-        
-        # Access the related ProductSubCat instance
         product_subcat = product.productsubcat
         product_subcat.product_price = request.POST.get('product_price')
         product_subcat.product_image = request.POST.get('product_image')
         product_subcat.product_model = request.POST.get('product_model')
         product_subcat.product_ram = request.POST.get('product_ram')
-        
-        
         product_subcat.save()
         product.save()
         
@@ -48,11 +40,8 @@ def add_product(request):
         product_model = request.POST.get('product_model')
         product_ram = request.POST.get('product_ram')
 
-        if product_name:  # Check if a product name is provided
-            # Create ProductMst instance
+        if product_name:
             product_mst = ProductMst.objects.create(product_name=product_name)
-            
-            # Create ProductSubCat instance
             product_subcat = ProductSubCat.objects.create(
                 product=product_mst,
                 product_price=product_price,
